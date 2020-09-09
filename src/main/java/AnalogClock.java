895851d4f307813154a67421ab32ee1f5e059b98
Jeremy Brandon Xavier Co <jeremyb.228217@nv.ccsd.net>
2020-09-09 16:28:29 +0000

    Copied all previous code from before

--- src/main/java/AnalogClock.java
+++ src/main/java/AnalogClock.java
@@ -7,11 +7,19 @@ class AnalogClock {
         Scanner scanner = new Scanner(System.in);
 
         //Reads ints from user
-
+        int deg = scanner.nextInt();
+        int hour;
+        int ndeg;
+        int aTime;
 
         /*
          *  your code goes here
          */
+        hour = deg / 30;
+        ndeg = deg - (hour * 30);
+        aTime = ndeg * 2;
+        System.out.print(aTime + " min");
+
 
         // closing the scanner object
         scanner.close();
--- src/main/java/Timestamps.java
+++ src/main/java/Timestamps.java
@@ -13,12 +13,21 @@ class Timestamps {
         int hours2 = scanner.nextInt();
         int minutes2 = scanner.nextInt();
         int seconds2 = scanner.nextInt();
-
+        int secInHour1, secInHour2;
+        int secInMin1, secInMin2;
 
         /*
          *  your code goes here
          */
-
+        secInHour1 = hours1 * 3600;
+        secInHour2 = hours2 * 3600;
+        secInMin1 = minutes1 * 60;
+        secInMin2 = minutes2 * 60;
+        int deltaHour = secInHour2 - secInHour1;
+        int deltaMin = secInMin2 - secInMin1;
+        int delta = seconds2 - seconds1;
+        int totalSec = deltaHour + deltaMin + delta;
+        System.out.print(totalSec);
 
         // closing the scanner object
         scanner.close();
